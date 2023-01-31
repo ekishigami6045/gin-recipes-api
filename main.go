97dad53aafe86239e4d29e2cee721a0ca0939d0b
eijikishigami <e.kishigami@gmail.com>
2023-01-31 17:24:30 +0900

    recipe data model

--- main.go
+++ main.go
@@ -1,6 +1,17 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Recipe struct {
+	Name        string    `json:"name"`
+	Tags        []string  `json:"tags"`
+	Ingredients []string  `json:"ingredients"`
+	PublishedAt time.Time `json:"publishedAt"`
+}
 
 func main() {
 	router := gin.Default()
